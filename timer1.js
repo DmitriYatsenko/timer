a82b48fe47ec7998e8be5ec3db17c85c4ff492b2
Dmitri Yatsenko <dmitriyatsenko1995@gmail.com>
2021-12-01 23:13:47 +0000

    fixed

--- timer1.js
+++ timer1.js
@@ -1,22 +1,26 @@
-const time = [2, 5, 8];
+//const time = [2, 5, 8];
+const time = process.argv.slice(2);
+let latestTime = Math.max(...time.map(t => parseInt(t)));
 
 const alarm = function (time) {
-    let delay = 1000;
-    for (let w = 0; w < 10; w++) {
+    let delay = 0;
+    for (let w = 0; w <= latestTime; w++) {
         setTimeout(() => {
             let v = false;
             for (let u = 0; u < time.length; u++) {
-                if (w === time[u]) {
+                if (w === parseInt(time[u])) {
                     v = true;
                 }
             }
             if (v) {
                 process.stdout.write('\x07');
+                console.log('beep-beep!')
             }
-            console.log(w);
+            else {
+                console.log(w);
+            }
         }, delay);
         delay += 1000;
     }
 }
-
 alarm(time);
